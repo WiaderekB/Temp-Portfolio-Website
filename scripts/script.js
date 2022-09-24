3d23bae24c558383408d5a38ad39752dd54c5b61
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2022-09-24 19:52:07 +0200

    Lock screen while nav is open

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,15 @@ var x = document.getElementById("nav_buttons");
 function toggleNav() {
 	if (x.classList.contains("nav_active")) {
 		x.classList.remove("nav_active");
+		document.body.style.overflowY = "scroll";
+		document.getElementsByTagName("html")[0].style.overflowY = "scroll";
 		setTimeout(function () {
 			x.style.zIndex = "-1";
 		}, 500);
 	} else {
 		x.classList.add("nav_active");
 		x.style.zIndex = "1";
+		document.body.style.overflowY = "hidden";
+		document.getElementsByTagName("html")[0].style.overflowY = "hidden";
 	}
 }
